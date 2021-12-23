44d97e11bcfd64fe7207c96dfbc0a00169038b99
fanggfeii <fangfei0623@163.com>
2021-12-23 15:14:30 +0800

    test

--- demo_三目运算符.py
+++ demo_三目运算符.py
@@ -9,7 +9,7 @@ exp1 if contion else exp2
 使用 if else 实现三目运算符（条件运算符）的格式如下：
 exp1 if contion else exp2
 
-condition 是判断条件，exp1 和 exp2 是两个表达式。
+condition 是判断条件，exp1 和 exp2 是两个表达式。...
 如果 condition 成立（结果为真），就执行 exp1，并把 exp1 的结果作为整个表达式的结果；
 如果 condition 不成立（结果为假），就执行 exp2，并把 exp2 的结果作为整个表达式的结果。
 '''
